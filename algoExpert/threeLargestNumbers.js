0ceeeaec038bb8586ffdedc804f964c0f02a686c
samuel-casey <48266366+samuel-casey@users.noreply.github.com>
2020-12-05 17:33:32 -0500

    added solution1 for threeLargestNumbers.js

---
+++ algoExpert/threeLargestNumbers.js
@@ -0,0 +1,24 @@
+function findThreeLargestNumbers(array) {
+  const result = []
+	
+	for (let i of array) {
+			if (result.length < 3) {
+				result.push(i)
+			} else if (result.length === 3) {
+				result.sort((a, b) => a - b)
+				if (i >= result[2]) {
+					result.shift()
+					result.push(i)
+				} else if (i >= result[0] && i < result[1]) {
+					result.shift()
+					result.unshift(i)
+				} else if (i >= result[1] && i < result[2]) {
+					result.shift()
+					result.unshift(i)
+				} 
+			}
+		}
+
+return result.sort((a,b) => a - b)
+}
+
